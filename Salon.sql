fa5a6c2d9b00f2608107a9229bcbdf85416e8705
Lsasko <luka.sasko123@gmail.com>
2024-02-08 18:19:27 +0100

    Changed SERIAL into INTEGER in PK

--- Salon.sql
+++ Salon.sql
@@ -7,7 +7,7 @@ CREATE TABLE "Owner"(
 	
 );
 CREATE TABLE "Salon"(
-	"Id" SERIAL PRIMARY KEY,
+	"Id" integer PRIMARY KEY,
 	"OwnerId" integer,
 	"Name" text NOT NULL,
 	"Year" date,
@@ -15,13 +15,13 @@ CREATE TABLE "Salon"(
 	CONSTRAINT "FK_Owner_Salon_OwnerId" FOREIGN KEY("OwnerId") REFERENCES "Owner"("Id")
 );
 CREATE TABLE "Service"(
-	"Id" SERIAl PRIMARY KEY,
+	"Id" integer PRIMARY KEY,
 	"Name" text NOT NULL,
 	"Price" integer NOT NULL,
 	"AvailableAppointment" date NOT NULL
 );
 CREATE TABLE "SalonService"(
-	"Id" SERIAL PRIMARY KEY,
+	"Id" integer PRIMARY KEY,
 	"SalonId" integer NOT NULL,
 	"ServiceId" integer NOT NULL,
 	"Date" date NOT NULL,
@@ -29,14 +29,14 @@ CREATE TABLE "SalonService"(
 	CONSTRAINT "FK_Service_SalonService_ServiceId" FOREIGN KEY ("ServiceId") REFERENCES "Service"("Id")
 );
 CREATE TABLE "Client"(
-	"Id" SERIAL PRIMARY KEY,
+	"Id" integer PRIMARY KEY,
 	"FirstName" text NOT NULL,
 	"LastName" text NOT NULL,
 	"Contact" text NOT NULL,
 	"Email" text
 );
 CREATE TABLE "Reservation"(
-	"Id" SERIAL PRIMARY KEY,
+	"Id" integer PRIMARY KEY,
 	"ClientId" integer NOT NULL,
 	"SalonServiceId" integer NOT NULL,
 	CONSTRAINT "FK_Client_Reservation_ClientId"  FOREIGN KEY ("ClientId") REFERENCES "Client"("Id"),
@@ -49,63 +49,66 @@ VALUES ('0','Owner1','1','123456789');
 INSERT INTO "Owner" ("Id","FirstName","LastName","Contact")
 VALUES ('1','Owner2','2','123456789');
 
-INSERT INTO "Salon" ("OwnerId","Name","Year","Adress")
-VALUES ('0','Salon1','2015-10-10','Adress1');
-INSERT INTO "Salon" ("OwnerId","Name","Year","Adress")
-VALUES ('0','Salon2','2017-10-10','Adress2');
-INSERT INTO "Salon" ("OwnerId","Name","Year","Adress")
-VALUES ('1','Salon3','2019-10-10','Adress3');
+INSERT INTO "Salon" ("Id","OwnerId","Name","Year","Adress")
+VALUES ('1','0','Salon1','2015-10-10','Adress1');
+INSERT INTO "Salon" ("Id","OwnerId","Name","Year","Adress")
+VALUES ('2','0','Salon2','2017-10-10','Adress2');
+INSERT INTO "Salon" ("Id","OwnerId","Name","Year","Adress")
+VALUES ('3','1','Salon3','2019-10-10','Adress3');
 
-INSERT INTO "Service" ("Name","Price","AvailableAppointment")
-VALUES ('Service1','50','2024-02-08');
-INSERT INTO "Service" ("Name","Price","AvailableAppointment")
-VALUES ('Service2','150','2024-02-10');
-INSERT INTO "Service" ("Name","Price","AvailableAppointment")
-VALUES ('Service3','250','2024-02-12');
-INSERT INTO "Service" ("Name","Price","AvailableAppointment")
-VALUES ('Service4','25','2024-02-7');
+INSERT INTO "Service" ("Id","Name","Price","AvailableAppointment")
+VALUES ('1','Service1','50','2024-02-08');
+INSERT INTO "Service" ("Id","Name","Price","AvailableAppointment")
+VALUES ('2','Service2','150','2024-02-10');
+INSERT INTO "Service" ("Id","Name","Price","AvailableAppointment")
+VALUES ('3','Service3','250','2024-02-12');
+INSERT INTO "Service" ("Id","Name","Price","AvailableAppointment")
+VALUES ('4','Service4','25','2024-02-7');
 
-INSERT INTO "SalonService" ("SalonId","ServiceId","Date")
-VALUES ('10','9','2024-02-08');
-INSERT INTO "SalonService" ("SalonId","ServiceId","Date")
-VALUES ('10','9','2024-02-18');
-INSERT INTO "SalonService" ("SalonId","ServiceId","Date")
-VALUES ('11','10','2024-02-08');
-INSERT INTO "SalonService" ("SalonId","ServiceId","Date")
-VALUES ('11','10','2024-02-18');
-INSERT INTO "SalonService" ("SalonId","ServiceId","Date")
-VALUES ('12','11','2024-02-28');
-INSERT INTO "SalonService" ("SalonId","ServiceId","Date")
-VALUES ('12','12','2024-02-21');
+INSERT INTO "SalonService" ("Id","SalonId","ServiceId","Date")
+VALUES ('1','1','1','2024-02-08');
+INSERT INTO "SalonService" ("Id","SalonId","ServiceId","Date")
+VALUES ('2','1','2','2024-02-18');
+INSERT INTO "SalonService" ("Id","SalonId","ServiceId","Date")
+VALUES ('3','2','3','2024-02-08');
+INSERT INTO "SalonService" ("Id","SalonId","ServiceId","Date")
+VALUES ('4','2','4','2024-02-18');
+INSERT INTO "SalonService" ("Id","SalonId","ServiceId","Date")
+VALUES ('5','3','1','2024-02-28');
+INSERT INTO "SalonService" ("Id","SalonId","ServiceId","Date")
+VALUES ('6','3','2','2024-02-21');
 
 
-INSERT INTO "Client" ("FirstName","LastName","Contact","Email")
-VALUES ('Client1','1','123456','Client1@gmail.com');
-INSERT INTO "Client" ("FirstName","LastName","Contact","Email")
-VALUES ('Client2','2','123455','Client2@gmail.com');
-INSERT INTO "Client" ("FirstName","LastName","Contact","Email")
-VALUES ('Client3','3','123556','Client3@gmail.com');
-INSERT INTO "Client" ("FirstName","LastName","Contact","Email")
-VALUES ('Client4','4','124456','Client4@gmail.com');
+INSERT INTO "Client" ("Id","FirstName","LastName","Contact","Email")
+VALUES ('1','Client1','ab','123456','Client1@gmail.com');
+INSERT INTO "Client" ("Id","FirstName","LastName","Contact","Email")
+VALUES ('2','Client2','ab','123455','Client2@gmail.com');
+INSERT INTO "Client" ("Id","FirstName","LastName","Contact","Email")
+VALUES ('3','Client3','ac','123556','Client3@gmail.com');
+INSERT INTO "Client" ("Id","FirstName","LastName","Contact","Email")
+VALUES ('4','Client4','ad','124456','Client4@gmail.com');
 
-INSERT INTO "Reservation" ("ClientId","SalonServiceId")
-VALUES ('1','3');
-INSERT INTO "Reservation" ("ClientId","SalonServiceId")
-VALUES ('2','4');
-INSERT INTO "Reservation" ("ClientId","SalonServiceId")
-VALUES ('3','5');
-INSERT INTO "Reservation" ("ClientId","SalonServiceId")
-VALUES ('4','6');
-INSERT INTO "Reservation" ("ClientId","SalonServiceId")
-VALUES ('4','7');
-INSERT INTO "Reservation" ("ClientId","SalonServiceId")
-VALUES ('4','8');
+INSERT INTO "Reservation" ("Id","ClientId","SalonServiceId")
+VALUES ('1','1','1');
+INSERT INTO "Reservation" ("Id","ClientId","SalonServiceId")
+VALUES ('2','2','2');
+INSERT INTO "Reservation" ("Id","ClientId","SalonServiceId")
+VALUES ('3','3','3');
+INSERT INTO "Reservation" ("Id","ClientId","SalonServiceId")
+VALUES ('4','4','4');
+INSERT INTO "Reservation" ("Id","ClientId","SalonServiceId")
+VALUES ('5','4','5');
+INSERT INTO "Reservation" ("Id","ClientId","SalonServiceId")
+VALUES ('6','4','6');
 */
-/*
 UPDATE "Client"
 SET "FirstName" = 'Alfred'
 WHERE "Id" = 3;
-*/
+
+SELECT "Client"."FirstName", "Client"."Contact"
+FROM "Client"
+WHERE "Id" = 3;
+
 
 SELECT "Salon"."Name", "Owner"."FirstName", "Owner"."LastName"
 FROM "Salon"
